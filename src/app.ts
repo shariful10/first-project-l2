871ccaf891210b62c960c3c94f2aae7563d9692a
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-05-01 00:06:26 +0600

    welcome page design

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send(
+    `<div style="background: blue; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); width: 500px; height: 200px; margin: auto; margin-top: 50px; display: flex; flex-direction: column; justify-content: center; align-items: cenrer;"><h1 style="color: white; text-align: center;">Welcome to the server 🥳</h1></div>`,
+  );
 });
 
 export default app;
