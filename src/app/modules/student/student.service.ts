d2a7ae3f2299ce7a67f0e8db4f163b69625426ed
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-05-02 01:02:43 +0600

    feature: Instance Method

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,8 +1,12 @@
 import { StudentModel } from "../student.model";
 import { IStudent } from "./student.interface";
 
-const createStudentInfoDB = async (student: IStudent) => {
-  const result = await StudentModel.create(student);
+const createStudentInfoDB = async (studentData: IStudent) => {
+  // const result = await StudentModel.create(student); // built in static method
+  // return result;
+
+  const student = new StudentModel(studentData);
+  const result = await student.save(); // built in instant method
   return result;
 };
 
