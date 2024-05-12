84ec18b4c092807f186cd1bdd8cb73981c67a415
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-05-13 00:56:29 +0600

    refector

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const ZFacultySchema = z.object({
   name: z.string({
-    required_error: "AcademicFaculty name is required",
+    required_error: "Academic Faculty name is required",
     invalid_type_error: "Academic Faculty name must be a string",
   }),
 });
