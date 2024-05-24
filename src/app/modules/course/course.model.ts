2e7978b8374a3db0a7faf2e8a58e035b759dea29
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-05-25 00:20:07 +0600

    Features: Couser interface & model

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+import { TCourse, TPreRequisiteCourses } from "./course.interface";
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: { type: Schema.Types.ObjectId },
+  isDeleted: { type: Boolean, default: false },
+});
+
+const courseSchema = new Schema<TCourse>({
+  title: { type: String, required: true, trim: true, unique: true },
+  prefix: { type: String, required: true, trim: true },
+  code: { type: Number, required: true, trim: true },
+  credits: { type: Number, required: true, trim: true },
+  preRequisiteCourses: [preRequisiteCoursesSchema],
+});
+
+export const Course = model<TCourse>("Course", courseSchema);
